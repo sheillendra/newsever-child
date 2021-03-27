5124745f8f1e63778d8c29c038827e075e832b77
sheillendra <suryana869@gmail.com>
2021-03-27 16:47:12 +0800

    refactor

--- functions.php
+++ functions.php
@@ -1,168 +1,30 @@
 <?php
-error_log('child');
-include_once(ABSPATH . 'wp-admin/includes/image.php');
+/*This file is part of newsever-child, newsever child theme.
 
-add_action('wp_enqueue_scripts', 'my_theme_enqueue_styles');
-function my_theme_enqueue_styles()
-{
-	$parenthandle = 'parent-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
-	$theme = wp_get_theme();
-	wp_enqueue_style(
-		$parenthandle,
-		get_template_directory_uri() . '/style.css',
-		array(),  // if the parent theme code has a dependency, copy it to here
-		$theme->parent()->get('Version')
-	);
-	wp_enqueue_style(
-		'child-style',
-		get_stylesheet_uri(),
-		array($parenthandle),
-		$theme->get('Version') // this only works if you have Version in the style header
-	);
+All functions of this file will be loaded before of parent theme functions.
+Learn more at https://codex.wordpress.org/Child_Themes.
+
+Note: this function loads the parent stylesheet before, then child theme stylesheet
+(leave it in place unless you know what you are doing.)
+*/
+
+if ( ! function_exists( 'suffice_child_enqueue_child_styles' ) ) {
+	function newsever_child_enqueue_child_styles() {
+	    // loading parent style
+	    wp_register_style(
+	      'parente2-style',
+	      get_template_directory_uri() . '/style.css'
+	    );
+
+	    wp_enqueue_style( 'parente2-style' );
+	    // loading child style
+	    wp_register_style(
+	      'childe2-style',
+	      get_stylesheet_directory_uri() . '/style.css'
+	    );
+	    wp_enqueue_style( 'childe2-style');
+	 }
 }
+add_action( 'wp_enqueue_scripts', 'newsever_child_enqueue_child_styles' );
 
-function my_comment_time_ago_function()
-{
-	return sprintf(esc_html__('%s lalu', 'newsever'), human_time_diff(get_comment_time('U'), current_time('timestamp')));
-}
-add_filter('get_comment_date', 'my_comment_time_ago_function');
-
-function my_post_time_ago_function()
-{
-	return sprintf(esc_html__('%s lalu', 'newsever'), human_time_diff(get_the_time('U'), current_time('timestamp')));
-}
-add_filter('the_time', 'my_post_time_ago_function');
-
-function auto_featured_image()
-{
-	global $post;
-	$attached_image = get_children("post_parent=$post->ID&amp;post_type=attachment&amp;post_mime_type=image&amp;numberposts=1");
-	if ($attached_image) {
-		// foreach ($attached_image as $attachment_id => $attachment) {
-		// 	// echo $attachment_id;
-		// 	if (wp_get_attachment_image($attachment_id) === '') {
-		// 		// echo $attachment_id;
-		// 		set_post_thumbnail($post->ID, $attachment_id);
-		// 	}
-		// }
-	} else {
-		$matches = array();
-		$output = preg_match_all('/<img.+?src=[\'"]([^\'"]+)[\'"].*?>/i', $post->post_content, $matches);
-		// print_r($matches);
-		if (isset($matches[1]) && isset($matches[1][0])) {
-			$firstImg = $matches[1][0];
-			$oldImgPath = str_replace('https://tribratanews.gorontalo.polri.go.id/', '../', $firstImg);
-			if (file_exists($oldImgPath)) {
-				$uploaddir = wp_upload_dir();
-				$pathinfo = pathinfo($firstImg);
-				$uploadfile = $uploaddir['path'] . '/' . $pathinfo['basename'];
-				copy($oldImgPath, $uploadfile);
-
-				$wp_filetype = wp_check_filetype($pathinfo['basename'], null);
-				$attachment = array(
-					'post_mime_type' => $wp_filetype['type'],
-					'post_title' => $pathinfo['filename'],
-					'post_content' => '',
-					'post_status' => 'inherit',
-				);
-
-				$attach_id = wp_insert_attachment($attachment, $uploadfile);
-
-				$imagenew = get_post($attach_id);
-				$fullsizepath = get_attached_file($imagenew->ID);
-				$attach_data = wp_generate_attachment_metadata($attach_id, $fullsizepath); // wp_generate_attachment_metadata($attach_id, $fullsizepath);
-				wp_update_attachment_metadata($attach_id, $attach_data);
-				set_post_thumbnail($post->ID, $attach_id);
-				$post->post_content = str_replace($matches[0][0], '', $post->post_content);
-				wp_update_post($post);
-			}
-		}
-	}
-}
-
-// Use it temporary to generate all featured images
-// add_action('the_post', 'auto_featured_image');
-// Used for new posts
-// add_action('save_post', 'auto_featured_image');
-// add_action('draft_to_publish', 'auto_featured_image');
-// add_action('new_to_publish', 'auto_featured_image');
-// add_action('pending_to_publish', 'auto_featured_image');
-// add_action('future_to_publish', 'auto_featured_image');
-
-/**
- * style untuk admin
- */
-// function my_admin_styles()
-// {
-// 	wp_enqueue_style('admin-styles', get_stylesheet_directory_uri() . '/assets/css/mystyle.css');
-// }
-// add_action('admin_enqueue_scripts', 'my_admin_styles');
-
-$custom_logo_id = get_theme_mod('custom_logo');
-// We have a logo. Logo is go.
-if ($custom_logo_id) {
-	function my_login_logo()
-	{
-		global $custom_logo_id;
-		$image = wp_get_attachment_image_src($custom_logo_id, 'full');
-?>
-		<style type="text/css">
-			#login h1 a,
-			.login h1 a {
-				background-image: url(<?php echo $image[0]; ?>);
-				height: 65px;
-				width: 320px;
-				background-size: 320px 65px;
-				background-repeat: no-repeat;
-			}
-		</style>
-<?php
-	}
-	add_action('login_enqueue_scripts', 'my_login_logo');
-}
-require get_stylesheet_directory() . '/inc/hooks/hook-front-page-banner-trending-posts.php';
-require get_stylesheet_directory() . '/inc/hooks/hook-single-header.php';
-require get_stylesheet_directory() . '/inc/hooks/hook-front-page.php';
-require get_stylesheet_directory() . '/inc/template-functions.php';
-
-
-if ( ! function_exists( 'pvc_post_views' ) ) {
-
-	function pvc_post_views( $post_id = 0, $echo = true ) {
-		// get all data
-		$post_id = (int) ( empty( $post_id ) ? get_the_ID() : $post_id );
-		$options = Post_Views_Counter()->options['display'];
-		$views = pvc_get_post_views( $post_id );
-
-		// prepare display
-		$label = apply_filters( 'pvc_post_views_label', ( function_exists( 'icl_t' ) ? icl_t( 'Post Views Counter', 'Post Views Label', $options['label'] ) : $options['label'] ), $post_id );
-
-		// get icon class
-		$icon_class = ( $options['icon_class'] !== '' ? esc_attr( $options['icon_class'] ) : '' );
-
-		// add dashicons class if needed
-		$icon_class = strpos( $icon_class, 'dashicons' ) === 0 ? 'dashicons ' . $icon_class : $icon_class;
-
-		// prepare icon output
-		$icon = apply_filters( 'pvc_post_views_icon', '<span class="post-views-icon ' . $icon_class . '"></span>', $post_id );
-
-		$html = apply_filters(
-			'pvc_post_views_html',
-			'<span class="post-views post-' . $post_id . ' entry-meta">
-				' . ( $options['display_style']['icon'] && $icon_class !== '' ? $icon : '' ) . '
-				' . ( $options['display_style']['text'] && $label !== '' ? '<span class="post-views-label">' . $label . ' </span>' : '' ) . '
-				<span class="post-views-count">' . number_format_i18n( $views ) . '</span>
-			</span>',
-			$post_id,
-			$views,
-			$label,
-			$icon
-		);
-
-		if ( $echo )
-			echo $html;
-		else
-			return $html;
-	}
-
-}
+/*Write here your own functions */
--- readme.md
+++
@@ -1,1 +0,0 @@
-# newsever-child
